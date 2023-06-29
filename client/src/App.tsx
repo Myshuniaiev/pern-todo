d5989fe5e1208b142a7f8b38ff50049b0eefd736
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2023-06-29 16:28:59 -0400

    Installation of the MUI and creation of the components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 /* Roboto Fonts */
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+/* Components */
+import Header from "./components/Header";
+import HomePage from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HomePage />  
     </div>
   );
 }
---
+++ client/src/components/CreateTodo/index.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Stack } from "@mui/material";
+
+interface IProps {
+  open: boolean;
+  onClose: Function;
+  onSubmit: Function;
+}
+
+export default function CreateTodoModal({ open, onClose, onSubmit }: IProps) {
+  return (
+    <Dialog open={open} onClose={() => onClose()}>
+      <DialogTitle>Create New todo</DialogTitle>
+      <DialogContent>
+        <Stack direction="column" spacing={3}>
+          <TextField id="name" fullWidth label="Title" variant="filled" />
+          <TextField
+            id="description"
+            fullWidth
+            label="Description"
+            variant="filled"
+          />
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => onClose()}>Cancel</Button>
+        <Button onClick={() => onSubmit()} variant="contained">
+          Create
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
